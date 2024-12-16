527b248541b355a4dc9fee1317bab4f4354d93f0
NewUser09 <98262514+NewUser09@users.noreply.github.com>
2024-12-16 13:36:21 +0700

    Update Nomor 1.py

--- Nomor 1.py
+++ Nomor 1.py
@@ -1,3 +1,5 @@
+#Nomor 1: buat kode python untuk menghitung jumlah bilangan dari 1 hingga bilangan n
+print("Menghitung jumlah bilangan dari 1 hingga n")
 n = int(input("Masukan nilai n : "))
 total = 0
 i = 1
